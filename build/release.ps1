cd34bfc9438ed5282b301753f1c8f4092625d447
kinshuk.bairagi <kinshuk.bairagi@flipkart.com>
2019-11-13 18:36:22 +0530

    docker build v1

---
+++ build/release.ps1
@@ -0,0 +1,66 @@
+# Build Script...
+
+Write-Host 'Hello, World!'
+scoop install unzip make
+ 
+
+cd C:\Code
+
+git config --global url.https://github.com/.insteadOf git@github.com:
+git clone --recurse-submodules -j8 https://github.com/voiceip/tinyphone.git
+cd C:\Code\tinyphone\
+
+subst E: C:\Code\tinyphone
+
+
+cd E:\lib\curl\
+.\buildconf.bat
+cd E:\lib\curl\winbuild
+
+nmake /f Makefile.vc mode=dll VC=15 DEBUG=no
+
+cd E:\lib\curl\builds
+cmd /k MKLINK /D E:\lib\curl\builds\libcurl-vc-x86-release-dll-ipv6-sspi-winssl E:\lib\curl\builds\libcurl-vc15-x86-release-dll-ipv6-sspi-winssl
+ls E:\lib\curl\builds
+cmd /k .\libcurl-vc15-x86-release-dll-ipv6-sspi-winssl\bin\curl.exe https://wttr.in/seattle
+
+
+#great 
+
+
+#G729
+cd E:\lib\bcg729\build\
+cmake ..
+msbuild /m bcg729.sln /p:Configuration=Debug /p:Platform=Win32
+
+
+cd E:\lib\cryptopp
+msbuild /m cryptest.sln -target:cryptlib:Rebuild /p:Configuration=Debug /p:Platform=Win32
+
+$wc = New-Object net.webclient; $wc.Downloadfile("https://download.steinberg.net/sdk_downloads/asiosdk_2.3.3_2019-06-14.zip", "E:\lib\portaudio\src\hostapi\asio\asiosdk_2.3.3_2019-06-14.zip")
+cd E:\lib\portaudio\src\hostapi\asio
+unzip asiosdk_2.3.3_2019-06-14.zip
+mv asiosdk_2.3.3_2019-06-14 ASIOSDK
+cd E:\lib\portaudio\build\msvc
+msbuild /m portaudio.sln /p:Configuration=Debug /p:Platform=Win32
+
+cd E:\lib\pjproject
+msbuild /m pjproject-vs14.sln -target:libpjproject:Rebuild /p:Configuration=Debug-Static /p:Platform=Win32
+
+cd E:\lib\statsd-cpp
+cmake .
+msbuild /m statsd-cpp.vcxproj /p:Configuration=Debug /p:Platform=Win32
+
+
+
+#ls E:\lib\curl\builds\libcurl-vc-x86-release-dll-ipv6-sspi-winssl
+cd E:\tinyphone
+msbuild /m tinyphone.sln -target:tinyphone /p:Configuration=Debug /p:Platform=x86
+
+cd \Code\tinyphone\lib\json
+mkdir build 
+cd build
+cmake ..
+cmake --build .
+
+
