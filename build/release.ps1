971f9abadbba45355818bbbe79af0b92a45fd237
kinshuk.bairagi <kinshuk.bairagi@flipkart.com>
2019-11-13 23:45:06 +0530

    update build instructions

--- build/release.ps1
+++ build/release.ps1
@@ -8,7 +8,7 @@ cd C:\Code
 
 git config --global url.https://github.com/.insteadOf git@github.com:
 git clone --recurse-submodules -j8 https://github.com/voiceip/tinyphone.git
-cd C:\Code\tinyphone\
+#cd C:\Code\tinyphone\
 
 subst E: C:\Code\tinyphone
 
@@ -20,9 +20,9 @@ cd E:\lib\curl\winbuild
 nmake /f Makefile.vc mode=dll VC=15 DEBUG=no
 
 cd E:\lib\curl\builds
-cmd /k MKLINK /D E:\lib\curl\builds\libcurl-vc-x86-release-dll-ipv6-sspi-winssl E:\lib\curl\builds\libcurl-vc15-x86-release-dll-ipv6-sspi-winssl
+cmd /c MKLINK /D E:\lib\curl\builds\libcurl-vc-x86-release-dll-ipv6-sspi-winssl E:\lib\curl\builds\libcurl-vc15-x86-release-dll-ipv6-sspi-winssl
 ls E:\lib\curl\builds
-cmd /k .\libcurl-vc15-x86-release-dll-ipv6-sspi-winssl\bin\curl.exe https://wttr.in/seattle
+cmd /c .\libcurl-vc15-x86-release-dll-ipv6-sspi-winssl\bin\curl.exe https://wttr.in/seattle
 
 
 #great 
@@ -35,7 +35,7 @@ msbuild /m bcg729.sln /p:Configuration=Debug /p:Platform=Win32
 
 
 cd E:\lib\cryptopp
-msbuild /m cryptest.sln -target:cryptlib:Rebuild /p:Configuration=Debug /p:Platform=Win32
+msbuild /m cryptlib.vcxproj /p:Configuration=Debug /p:Platform=Win32 /p:PlatformToolset=v140_xp
 
 $wc = New-Object net.webclient; $wc.Downloadfile("https://download.steinberg.net/sdk_downloads/asiosdk_2.3.3_2019-06-14.zip", "E:\lib\portaudio\src\hostapi\asio\asiosdk_2.3.3_2019-06-14.zip")
 cd E:\lib\portaudio\src\hostapi\asio
@@ -55,12 +55,8 @@ msbuild /m statsd-cpp.vcxproj /p:Configuration=Debug /p:Platform=Win32
 
 #ls E:\lib\curl\builds\libcurl-vc-x86-release-dll-ipv6-sspi-winssl
 cd E:\tinyphone
-msbuild /m tinyphone.sln -target:tinyphone /p:Configuration=Debug /p:Platform=x86
-
-cd \Code\tinyphone\lib\json
-mkdir build 
-cd build
-cmake ..
-cmake --build .
-
+#msbuild /m tinyphone.sln -target:tinyphone /p:Configuration=Debug /p:Platform=x86
+#msbuild /m tinyphone.sln -target:tinyphone:Rebuild /p:Configuration=Debug /p:Platform=x86
+msbuild /m tinyphone.sln /p:Configuration=Debug /p:Platform=x86
+ 
 
