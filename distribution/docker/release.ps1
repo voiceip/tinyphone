37e1140cbf30f850747891c7ad20b1ac14287600
Kinshuk Bairagi <hi@kinsh.uk>
2022-03-21 18:24:34 +0530

    2019

--- distribution/docker/release.ps1
+++ distribution/docker/release.ps1
@@ -17,20 +17,20 @@ cd E:\lib\curl\
 cd E:\lib\curl\winbuild
 
 where.exe msbuild.exe
-nmake /f Makefile.vc mode=dll VC=15 MACHINE=x86 DEBUG=no
+nmake /f Makefile.vc mode=dll VC=19 MACHINE=x86 DEBUG=no
 
-cmd /c MKLINK /D E:\lib\curl\builds\libcurl-vc-x86-release-dll-ipv6-sspi-winssl E:\lib\curl\builds\libcurl-vc15-x86-release-dll-ipv6-sspi-winssl
-cmd /c E:\lib\curl\builds\libcurl-vc15-x86-release-dll-ipv6-sspi-winssl\bin\curl.exe https://wttr.in/bangalore
+cmd /c MKLINK /D E:\lib\curl\builds\libcurl-vc-x86-release-dll-ipv6-sspi-winssl E:\lib\curl\builds\libcurl-vc19-x86-release-dll-ipv6-sspi-winssl
+cmd /c E:\lib\curl\builds\libcurl-vc19-x86-release-dll-ipv6-sspi-winssl\bin\curl.exe https://wttr.in/bangalore
 
 
 #G729
 cd E:\lib\bcg729\build\
-cmake ..
-msbuild /m bcg729.sln /p:Configuration=$BuildMode /p:Platform=Win32
+cmake .. -A Win32
+msbuild /m bcg729.sln /p:Configuration=$BuildMode /p:Platform=Win32 /p:PlatformToolset=v142
 
 #cryptopp
 cd E:\lib\cryptopp
-msbuild /m cryptlib.vcxproj /p:Configuration=$BuildMode /p:Platform=Win32 /p:PlatformToolset=v140_xp
+msbuild /m cryptlib.vcxproj /p:Configuration=$BuildMode /p:Platform=Win32 /p:PlatformToolset=v142
 
 #portaudio
 $wc = New-Object net.webclient; $wc.Downloadfile("https://download.steinberg.net/sdk_downloads/asiosdk_2.3.3_2019-06-14.zip", "E:\lib\portaudio\src\hostapi\asio\asiosdk_2.3.3_2019-06-14.zip")
@@ -38,16 +38,16 @@ cd E:\lib\portaudio\src\hostapi\asio
 unzip asiosdk_2.3.3_2019-06-14.zip
 mv asiosdk_2.3.3_2019-06-14 ASIOSDK
 cd E:\lib\portaudio\build\msvc
-msbuild /m portaudio.sln /p:Configuration=$BuildMode /p:Platform=Win32
+msbuild /m portaudio.sln /p:Configuration=$BuildMode /p:Platform=Win32 /p:PlatformToolset=v142 /p:WindowsTargetPlatformVersion=10.0
 
 #pjproject
 cd E:\lib\pjproject
-msbuild /m pjproject-vs14.sln -target:libpjproject:Rebuild /p:Configuration=$BuildMode-Static /p:Platform=Win32
+msbuild /m pjproject-vs14.sln -target:libpjproject:Rebuild /p:Configuration=$BuildMode-Static /p:Platform=Win32 /p:PlatformToolset=v142
 
 #statsd-cpp
 cd E:\lib\statsd-cpp
 cmake .
-msbuild /m statsd-cpp.vcxproj /p:Configuration=$BuildMode /p:Platform=Win32
+msbuild /m statsd-cpp.vcxproj /p:Configuration=$BuildMode /p:Platform=Win32 /p:PlatformToolset=v142 /p:WindowsTargetPlatformVersion=10.0
 
 #tinyphone
 cd E:\tinyphone
@@ -55,7 +55,7 @@ sed -i 's/stampver.inf.*\$/stampver.inf $/g' tinyphone.vcxproj
 
 #msbuild /m tinyphone.sln -target:tinyphone /p:Configuration=$BuildMode /p:Platform=x86
 #msbuild /m tinyphone.sln -target:tinyphone:Rebuild /p:Configuration=$BuildMode /p:Platform=x86
-msbuild /m tinyphone.sln /p:Configuration=$BuildMode /p:Platform=x86
+msbuild /m tinyphone.sln /p:Configuration=$BuildMode /p:Platform=x86 /p:PlatformToolset=v142 /p:WindowsTargetPlatformVersion=10.0
 
 
 #required for github-ci permission issue.
