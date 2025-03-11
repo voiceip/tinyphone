092fdd20a19f2036d3b8a0ae0af49cef594e5e3d
Kinshuk Bairagi <hi@kinsh.uk>
2025-03-11 10:58:13 +0530

    linux: shutdown on sigterm

--- tinyphone-linux/console.cpp
+++ tinyphone-linux/console.cpp
@@ -29,8 +29,18 @@ void Stop(){
     tp::StopApp();
 }
 
+void signal_handler(int signal) {
+    std::cout << "Received signal " << signal << std::endl;
+    Stop();
+    exit(signal);
+}
+
+
 int main(int argc, char *argv[])
 {
+     // Register signal handlers
+    signal(SIGINT, signal_handler);
+    signal(SIGTERM, signal_handler);
     std::cout << "Hello Tinyphone!\n";
     Start();
     return 0;
